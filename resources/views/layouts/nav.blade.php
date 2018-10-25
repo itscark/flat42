2878c36a1bea0c0f40ef102a582f16b40e41fe3a
marcpeternell <peternellmarc@gmail.com>
2018-10-25 17:35:12 +0200

    nav animation hinzugefügt

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -5,27 +5,27 @@
                 @svg('favicon-black-white')
             </a>
         </div>
-        <ul>
-            <li>
-                <a href="">Blog</a>
 
+        <ul class="buttons">
+            <li class="nav-btn">
+                <a href="" class="btn">Blog</a>
             </li>
 
-            <li>
-                <a href="">Über Uns</a>
+            <li class="nav-btn">
+                <a class="btn" href="">Über Uns</a>
             </li>
 
             @if(!auth()->check())
-                <li>
-                    <a href="{{ route('login') }}">Login</a>
+                <li class="nav-btn">
+                    <a class="btn" href="{{ route('login') }}">Login</a>
                 </li>
 
-                <li>
-                    <a href="{{ route('register') }}">Register</a>
+                <li class="nav-btn">
+                    <a class="btn" href="{{ route('register') }}">Register</a>
                 </li>
             @else
-                <li>
-                    <a href="{{ route('logout') }}">Logout</a>
+                <li class="nav-btn">
+                    <a class="btn" href="{{ route('logout') }}">Logout</a>
                 </li>
             @endif
         </ul>
