c35c1fd636f7a3cbd49a4cc1e027e1a7227c4967
marcpeternell <peternellmarc@gmail.com>
2019-01-21 21:16:42 +0100

    heroku setting

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,9 +16,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (App::environment('production')) {
-            URL::forceScheme('https');
-        }
+            URL::forceScheme('http');
         Schema::defaultStringLength(191);
     }
 
