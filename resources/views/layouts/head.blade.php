c717372ed1e332a6c2399b2262f33e40ea6bca76
marcpeternell <peternellmarc@gmail.com>
2019-01-29 16:58:05 +0100

    pwa

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -3,6 +3,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="manifest" href="{{ asset('manifest.json') }}">
+
     <title>Flat42</title>
 
     <!-- Favicon -->
