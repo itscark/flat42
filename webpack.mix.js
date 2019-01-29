d208e2c051058d70fb169884781b1520202cc481
marcpeternell <peternellmarc@gmail.com>
2019-01-29 20:23:56 +0100

    pwa working?

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,56 @@
 const mix = require('laravel-mix');
 const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
+const workboxPlugin = require('workbox-webpack-plugin');
+
+mix.js('resources/js/app.js', 'public/js')
+    .sourceMaps()
+    .sass('resources/sass/app.scss', 'public/css').options({
+    postCss: [
+        require('postcss-sorting')({
+            'properties-order': 'alphabetical'
+        })
+    ]
+})
+    .copy('resources/assets/images/', 'public/images/')
+    .copy('resources/assets/svg/', 'public/svg/');
+
+
+mix.webpackConfig({
+    plugins: [
+        new SWPrecacheWebpackPlugin({
+            cacheId: 'Flat42',
+            filename: 'service-worker.js',
+            staticFileGlobs: ['public/!**!/!*.{css,eot,svg,ttf,woff,woff2,js,html}'],
+            minify: true,
+            stripPrefix: '/',
+            handleFetch: true,
+            dynamicUrlToDependencies: {
+                '/': ['resources/views/layouts/master.blade.php'],
+            },
+            staticFileGlobsIgnorePatterns: [/\.map$/, /mix-manifest\.json$/, /manifest\.json$/, /service-worker\.js$/],
+            navigateFallback: '/',
+            runtimeCaching: [
+                {
+                    urlPattern: /^https:\/\/fonts\.googleapis\.com\//,
+                    handler: 'cacheFirst'
+                },
+                {
+                    urlPattern: /^https:\/\/www\.thecocktaildb\.com\/images\/media\/drink\/(\w+)\.jpg/,
+                    handler: 'cacheFirst'
+                }
+            ],
+        })
+    ]
+});
+
+
+
+
+/*
+
+const mix = require('laravel-mix');
+const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
+const workboxPlugin = require('workbox-webpack-plugin');
 
 mix.js('resources/js/app.js', 'public/js')
     .sourceMaps()
@@ -43,4 +94,4 @@ mix.webpackConfig({
             // importScripts: ['./js/push_message.js']
         })
     ]
-});
+});*/
