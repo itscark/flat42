cec86760349bec92b7cae344802b5e0ef34b5575
marcpeternell <peternellmarc@gmail.com>
2019-01-29 18:14:07 +0100

    pwa ready, iphone notch fixed

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -4,6 +4,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="theme-color" content="#13A399">
+    <meta name="apple-mobile-web-app-status-bar-style" content="default">
     <link rel="manifest" href="{{ asset('manifest.json') }}">
 
     <title>Flat42</title>
