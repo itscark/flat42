5eb616d7ff1cc88f95cdbf54c91ad1340353b286
marcpeternell <peternellmarc@gmail.com>
2019-01-29 19:43:02 +0100

    pwa working?

--- resources/views/layouts/scripts.blade.php
+++ resources/views/layouts/scripts.blade.php
@@ -1,4 +1,17 @@
-{{--<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.0.2/TweenMax.min.js"></script>
-script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.0.2/TimelineMax.min.js"></script>--}}
+<script>
+    if ('serviceWorker' in navigator ) {
+        window.addEventListener('load', function() {
+            navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+                // Registration was successful
+                console.log('ServiceWorker registration successful with scope: ', registration.scope);
+            }, function(err) {
+                // registration failed :(
+                console.log('ServiceWorker registration failed: ', err);
+            });
+        });
+    }
+</script>
+
+
 <script src="https://unpkg.com/vue-fullpage.js/dist/vue-fullpage.min.js"></script>
 <script src="{{ asset('js/app.js') }}"></script>
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 const mix = require('laravel-mix');
+const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
 
 mix.js('resources/js/app.js', 'public/js')
     .sourceMaps()
@@ -11,3 +12,35 @@ mix.js('resources/js/app.js', 'public/js')
 })
     .copy('resources/assets/images/', 'public/images/')
     .copy('resources/assets/svg/', 'public/svg/');
+
+
+mix.webpackConfig({
+    plugins: [
+        new SWPrecacheWebpackPlugin({
+            cacheId: 'pwa',
+            filename: 'service-worker.js',
+            staticFileGlobs: ['public/!**!/!*.{css,eot,svg,ttf,woff,woff2,js,html}'],
+            minify: true,
+            stripPrefix: '/',
+            handleFetch: true,
+            dynamicUrlToDependencies: { //you should add the path to your blade files here so they can be cached
+                //and have full support for offline first (example below)
+                '/': ['resources/views/layouts/master.blade.php'],
+                // '/posts': ['resources/views/posts.blade.php']
+            },
+            staticFileGlobsIgnorePatterns: [/\.map$/, /mix-manifest\.json$/, /manifest\.json$/, /service-worker\.js$/],
+            navigateFallback: '/',
+            runtimeCaching: [
+                {
+                    urlPattern: /^https:\/\/fonts\.googleapis\.com\//,
+                    handler: 'cacheFirst'
+                },
+                {
+                    urlPattern: /^https:\/\/www\.thecocktaildb\.com\/images\/media\/drink\/(\w+)\.jpg/,
+                    handler: 'cacheFirst'
+                }
+            ],
+            // importScripts: ['./js/push_message.js']
+        })
+    ]
+});
