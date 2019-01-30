980340e09a1d22b137c128175d1404c9563712b9
marcpeternell <peternellmarc@gmail.com>
2019-01-30 23:34:43 +0100

    bug fix

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -18,7 +18,10 @@ window.Form = Form;
 Vue.use(VueRouter);
 Vue.use(BootstrapVue);
 Vue.use(VueFlashMessage);
-Vue.use(VuejsDialog);
+Vue.use(VuejsDialog, {
+    okText: 'Fortfahren',
+    cancelText: 'Abbrechen',
+});
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 let token = document.head.querySelector('meta[name="csrf-token"]');
