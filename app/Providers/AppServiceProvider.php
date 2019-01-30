e92bb5179525f844429588609f79ad5ee42c1ca1
marcpeternell <peternellmarc@gmail.com>
2019-01-30 18:10:08 +0100

    heroku

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,7 +16,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        URL::forceScheme('httts');
+        URL::forceScheme('https');
         Schema::defaultStringLength(191);
     }
 
